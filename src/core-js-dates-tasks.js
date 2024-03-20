17356b52b9132567a3db727e8e865f0b404a220d
pambaka <pambaka@ya.ru>
2024-03-20 11:40:04 +0100

    feat: getCountDaysOnPeriod is implemented

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -130,8 +130,21 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const startDate = new Date(dateStart);
+  const endDate = new Date(dateEnd);
+
+  const startDateTimestamp = Date.parse(startDate);
+  const endDateTimestamp = Date.parse(endDate);
+
+  const dayDuration = 24 * 60 * 60 * 1000;
+
+  const endDayIncluded = 1;
+
+  const days =
+    (endDateTimestamp - startDateTimestamp) / dayDuration + endDayIncluded;
+
+  return days;
 }
 
 /**
